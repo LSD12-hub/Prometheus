dd95fddee759533eb993c654dfb20bdb0fbd0db5
LSD12-hub <69172592+LSD12-hub@users.noreply.github.com>
2021-05-13 21:09:05 +0200

    Add files via upload

--- prometheus.py
+++ prometheus.py
@@ -1,6 +1,8 @@
 import random
 
 grid = 3
+isRunning = True
+playerDirection
 player = {
 	'name' : 'p',
 	'x' : 1,
@@ -25,7 +27,14 @@ fire = {
 	'y' : random.randint(0, grid - 1),
 	'xp' : 25
 }
-
+treasure = {
+        'name1' : 't1',
+        'name2' : 't2',
+        'x1' : random.randint(0, grid - 1),
+        'x2' : random.randint(0, grid - 1),
+        'y1' : random.randint(0, grid - 1),
+        'y2' : random.randint(0, grid - 1),
+        }
 
 levels = {
         'level1' : [['-' for i in range(grid)], ['-' for i in range(grid)], ['-' for i in range(grid)]],
@@ -40,7 +49,7 @@ def generateCharacters(level):
 		levels.get(level)[wolf.get('x')][wolf.get('y')] = wolf.get('name')
 		levels.get(level)[fire.get('x')][fire.get('y')] = fire.get('name')
 	if level == 'level2':
-		grid = 4
+		grid = 3
 		levels.get(level)[player.get('x')][player.get('y')] = player.get('name')
 		levels.get(level)[wolf.get('x')][wolf.get('y')] = wolf.get('name')
 		levels.get(level)[fire.get('x')][fire.get('y')] = fire.get('name')
@@ -59,5 +68,77 @@ def main():
 		print('')
 	print('******************************\n')
 	
+	while isRunning:
+		gamePlay()
+
+def moveUp():
+	print('move up')
+	#make sure they move up
+	
+def moveDown():
+	print('move down')
+	#make sure they move down
+
+def moveRight():
+	print('move right')
+	#make sure they move right
+
+def moveLeft():
+	print('move left')
+	#make sure they move left
+
+def checkMove():
+        #Check if move can be made
+        #if can be made return false
+        #if not return true
+        return false
+
+#def fightRun():
+        #do the calualations for the Xp and all
+        
+def checkSqure():
+        #check if they are in line with the a fire or wolf or treasure
+        #if in line with wolf
+        print('WOLFFFFFF!\n')
+        print('1.FIGHT!')
+        choice = input('2.RUN ')
+        fightRun(choice)
+        
+        #if in line with fire
+        print('FIREEEEE!\n')
+        print('1.FIGHT!')
+        choice = input('2.RUN ')
+        fightRun(choice)
+        
+def checkItems():
+        #I will do this one
+        print('check')
+        
+def gamePlay():
+	print('XP: ' + str(player.get('xp')))
+	print('Location ' + str(player.get('x')) + ',' + str(player.get('y')))
+	print('1. UP')
+	print('2. DOWN')
+	print('3. LEFT')
+	print('4. RIGHT')
+	playerDirection = input('Please input a move: ')
+	print()
+	
+	while checkMove():
+                playerDirection = input('Sorry not valid move, please retry: ')
+                
+        if playerDirection == '1':
+                moveUp()
+        if playerDirection == '2':
+                moveDown()
+        if playerDirection == '3':
+                moveRight()
+        if playerDirection == '4':
+                moveLeft()
+
+        checkSquare()
+        checkItems()
+	print('XP: ' + str(player.get('xp')))
+	print('Location ' + str(player.get('x')) + ',' + str(player.get('y')))
 	
 main();
