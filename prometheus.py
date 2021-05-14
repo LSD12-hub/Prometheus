67f7a913e4b05f3da6ca3d6c127863d30f800ee0
LSD12-hub <69172592+LSD12-hub@users.noreply.github.com>
2021-05-14 02:18:40 +0200

    Add files via upload

--- prometheus.py
+++ prometheus.py
@@ -2,7 +2,7 @@ import random
 
 grid = 3
 isRunning = True
-playerDirection
+playerDirection = ''
 player = {
 	'name' : 'p',
 	'x' : 1,
@@ -38,7 +38,7 @@ treasure = {
 
 levels = {
         'level1' : [['-' for i in range(grid)], ['-' for i in range(grid)], ['-' for i in range(grid)]],
-	'level2' : [['-' for i in range(grid + 1)],['-' for i in range(grid + 1)],['-' for i in range(grid + 1)],['-' for i in range(grid + 1)]],
+	'level2' : [['-' for i in range(grid)], ['-' for i in range(grid)], ['-' for i in range(grid)]],
 	'level3' : [['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)]]
         }
 # 81206
@@ -50,14 +50,25 @@ def generateCharacters(level):
 		levels.get(level)[fire.get('x')][fire.get('y')] = fire.get('name')
 	if level == 'level2':
 		grid = 3
-		levels.get(level)[player.get('x')][player.get('y')] = player.get('name')
+		levels.get(level)[treasure.get('x1')][treasure.get('y1')] = treasure.get('name1')
+		levels.get(level)[treasure.get('x2')][treasure.get('y2')] = treasure.get('name2')
 		levels.get(level)[wolf.get('x')][wolf.get('y')] = wolf.get('name')
 		levels.get(level)[fire.get('x')][fire.get('y')] = fire.get('name')
+		levels.get(level)[player.get('x')][player.get('y')] = player.get('name')
+		
 	if level == 'level3':
 		grid = 5
 		levels.get(level)[player.get('x')][player.get('y')] = player.get('name')
 		levels.get(level)[wolf.get('x')][wolf.get('y')] = wolf.get('name')
 		levels.get(level)[fire.get('x')][fire.get('y')] = fire.get('name')
+
+def eee(level):
+        while True:
+                if 'f' in level.get(level) and 'p' in level.get(level) and 'w' in level.get(level):
+                        break
+                else:
+                        generateCharacters(level)
+                        
 	
 def main():
 	print('************level 1***********')
@@ -67,14 +78,16 @@ def main():
 			print(levels.get('level1')[i][j], end = '')
 		print('')
 	print('******************************\n')
-	
+        
+	generateCharacters('level2')
+	print(levels.get('level2'))
 	while isRunning:
 		gamePlay()
 
 def moveUp():
-	print('move up')
-	#make sure they move up
-	
+        checkMove(player.get('x') - 1, 0)
+        player['x'] = player.get('x') - 1
+        print( player.get('x'))
 def moveDown():
 	print('move down')
 	#make sure they move down
@@ -87,14 +100,14 @@ def moveLeft():
 	print('move left')
 	#make sure they move left
 
-def checkMove():
-        #Check if move can be made
-        #if can be made return false
-        #if not return true
-        return false
+def checkMove(x, y):
+        try:
+                levels.get('level2')[x][y] = player.get('name') 
+        except:
+                chooseMove('error') 
 
-#def fightRun():
-        #do the calualations for the Xp and all
+def fightRun():
+        print('Calculations for the players')
         
 def checkSqure():
         #check if they are in line with the a fire or wolf or treasure
@@ -113,7 +126,28 @@ def checkSqure():
 def checkItems():
         #I will do this one
         print('check')
-        
+
+def chooseMove(place):
+        if place == 'playgame':
+                playerDirection = input('Please input a move: ')
+        elif place == 'error':
+                playerDirection = input('Sorry not valid move, please retry: ')
+        while True:
+                if playerDirection == '1':
+                        moveUp()
+                        break
+                elif playerDirection == '2':
+                        moveRight()
+                        break
+                elif playerDirection == '3':
+                        moveLeft()
+                        break
+                elif playerDirection == '4':
+                        moveRight()
+                        break
+                else:
+                        playerDirection = input('Sorry not valid move, please retry: ')                
+
 def gamePlay():
 	print('XP: ' + str(player.get('xp')))
 	print('Location ' + str(player.get('x')) + ',' + str(player.get('y')))
@@ -121,24 +155,9 @@ def gamePlay():
 	print('2. DOWN')
 	print('3. LEFT')
 	print('4. RIGHT')
-	playerDirection = input('Please input a move: ')
+	chooseMove('playgame')
 	print()
-	
-	while checkMove():
-                playerDirection = input('Sorry not valid move, please retry: ')
-                
-        if playerDirection == '1':
-                moveUp()
-        if playerDirection == '2':
-                moveDown()
-        if playerDirection == '3':
-                moveRight()
-        if playerDirection == '4':
-                moveLeft()
-
-        checkSquare()
-        checkItems()
 	print('XP: ' + str(player.get('xp')))
-	print('Location ' + str(player.get('x')) + ',' + str(player.get('y')))
+	print('Location ' + str(player.get('x')) + ',' + str(player.get('y')) + '\n')
 	
 main();
