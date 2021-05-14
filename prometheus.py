c20591592925fc85ee09b92bdbf3b1aa2704b9a4
LSD12-hub <69172592+LSD12-hub@users.noreply.github.com>
2021-05-14 12:26:32 +0200

    Add files via upload

--- prometheus.py
+++ prometheus.py
@@ -3,6 +3,7 @@ import random
 grid = 3
 isRunning = True
 playerDirection = ''
+
 player = {
 	'name' : 'p',
 	'x' : 1,
@@ -34,6 +35,7 @@ treasure = {
         'x2' : random.randint(0, grid - 1),
         'y1' : random.randint(0, grid - 1),
         'y2' : random.randint(0, grid - 1),
+        'xp' : 20
         }
 
 levels = {
@@ -41,7 +43,7 @@ levels = {
 	'level2' : [['-' for i in range(grid)], ['-' for i in range(grid)], ['-' for i in range(grid)]],
 	'level3' : [['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)],['-' for i in range(grid + 2)]]
         }
-# 81206
+
 def generateCharacters(level):
 	if level == 'level1':		
 		grid = 3
@@ -71,35 +73,58 @@ def eee(level):
                         
 	
 def main():
-	print('************level 1***********')
-	generateCharacters('level1')
-	for i in range(len(levels.get('level1'))):
-		for j in range(len(levels.get('level1'))):
-			print(levels.get('level1')[i][j], end = '')
-		print('')
-	print('******************************\n')
-        
-	generateCharacters('level2')
-	print(levels.get('level2'))
-	while isRunning:
-		gamePlay()
+    print('************level 1***********')
+    generateCharacters('level1')
+    for i in range(len(levels.get('level1'))):
+	    for j in range(len(levels.get('level1'))):
+		    print(levels.get('level1')[i][j], end = '')
+	    print('')
+    print('******************************\n')
+
+    print('************level 2***********\n')
+    generateCharacters('level2')
+    print(levels.get('level2'))
+    while isRunning:
+    	gamePlay()
+
+    print('*****************************\n')
+    
+    print('***********level 3***********\n')
+    generateCharacteres('level3')
+    print(levels.get('level3'))
+    while isRunning:
+        gamePlay()
 
 def moveUp():
     checkMove(player.get('x') - 1, 0)
     player['x'] = player.get('x') - 1
-
+    checkCell()
+    isRunning = killGame()
 
 def moveDown():
     checkMove(player.get('x') + 1, 0)
     player['x'] = player.get('x') + 1 
+    checkCell()
+    isRunning = killGame()
 
 def moveRight():
     checkMove(0, player.get('y') + 1)
     player['y'] = player.get('y') + 1
+    checkCell()
+    isRunning = killGame()
 
 def moveLeft():
     checkMove(0, player.get('y') - 1)
     player['y'] = player.get('y') - 1
+    checkCell()
+    isRunning = killGame()
+
+def killGame():
+    print(levels.get('level2'))
+    for lev in levels.get('level2'):
+        if lev == 'w' or lev == 'f' or lev == 't1' or lev == 't2':
+            return True
+    return False
 
 def checkMove(x, y):
     if x not in range(0, grid):
@@ -107,21 +132,67 @@ def checkMove(x, y):
     
     if y not in range(0,grid):
         chooseMove('error')
-        
-def checkSqure():
-        #check if they are in line with the a fire or wolf or treasure
-        #if in line with wolf
-        print('WOLFFFFFF!\n')
+
+def fightOrRun(choice, NPC):
+    while NPC == 'wolf':
+        if choice == '1':
+            print("You beat the wolf -30 XP ")
+            player['xp'] = player.get('xp') - wolf.get('xp')
+            levels.get('level2')[player.get('x')][player.get('y')] = '-'
+            break
+        elif choice == '2' :
+            print("-1 Xp")
+            player['xp'] =player.get('xp')-1
+            player['x'] = 1
+            player['y'] = 1
+            break
+        else:
+            choice = input('Sorry not a valid move, Please try again: ')
+
+    while NPC == 'fire':
+        if choice == '1':
+            print("You beat the fire -25 XP ")
+            player['xp']=player.get('xp') - fire.get('xp')
+            levels.get('level2')[player.get('x')][player.get('y')] = '-'
+            break
+        elif choice == '2' :
+            print("-1 Xp")
+            player['xp'] = player.get('xp') -1
+            player['x'] = 1
+            player['y'] = 1
+            break
+        else:
+            choice = input('Sorry not a valid move, Please try again: ')
+
+def checkCell():
+    #check if they are in line with the a fire or wolf or treasure
+    if player.get('x') == wolf.get('x') and player.get('y') == wolf.get('y'):
+        print('\nWOLFFFFFF!\n')
         print('1.FIGHT!')
         choice = input('2.RUN ')
-        fightRun(choice)
+        fightOrRun(choice, 'wolf')
         
-        #if in line with fire
-        print('FIREEEEE!\n')
+    if player.get('x') == fire.get('x') and player.get('y') == fire.get('y'):
+        print('\nFIREEEEE!\n')
         print('1.FIGHT!')
         choice = input('2.RUN ')
-        fightRun(choice)
-        
+        fightOrRun(choice, 'fire')
+       
+    if player.get('x') == treasure.get('x1') and player.get('y') == treasure.get('y1'):
+        print('\nTREEEEEAAAAAASSSUUUURE\n')
+        print('You just got treasure! +20 XP\n')
+        #add player xp + 20
+        player['xp'] = player.get('xp') + treasure.get('xp')
+        levels.get('level2')[player.get('x')][player.get('y')] = '-'
+
+    if player.get('x') == treasure.get('x2') and player.get('y') == treasure.get('y2'):
+        print('\nTREEEEEAAAAAASSSUUUURE\n')
+        print('You just got treasure! +20 XP\n')
+        #add player xp + 20
+        player['xp'] = player.get('xp') + treasure.get('xp')
+        levels.get('level2')[player.get('x')][player.get('y')] = '-'
+
+
 def checkItems():
         #I will do this one
         print('check')
